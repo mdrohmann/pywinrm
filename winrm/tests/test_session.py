fdc22d56413acdb48793d45caccd1acda86dec99
Martin C Drohmann <martin@connectedsignals.com>
2017-03-28 11:54:58 -0700

    adapt tests

--- winrm/tests/test_session.py
+++ winrm/tests/test_session.py
@@ -9,7 +9,7 @@ def test_run_cmd(protocol_fake):
     r = s.run_cmd('ipconfig', ['/all'])
 
     assert r.status_code == 0
-    assert b'Windows IP Configuration' in r.std_out
+    assert 'Windows IP Configuration' in r.std_out
     assert len(r.std_err) == 0
 
 
